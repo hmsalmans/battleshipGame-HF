62d012b492b7ab97faa12485f32217dd23da0220
Salman <hmsalmans1@gmail.com>
2020-08-31 02:43:47 -0400

    battleshipt basic game version is ready

--- battleship.js
+++ battleship.js
@@ -7,6 +7,7 @@ var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
+
 while (isSunk == false) {
     guess = prompt("please guess the location ( 0-6 )");
     if (guess < 0 || guess > 6) {
@@ -16,16 +17,16 @@ while (isSunk == false) {
         if (guess == location1 || guess == location2 || guess == location3) {
             hits = hits + 1;
             alert("You have " + hits + " hits!");
-             if (hits === 3) {
-                 alert("Game over baby");
-                 isSunk === "true";
-             }
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sunk the battleship");
+            }
+
         } else {
             alert("Sorry you missed and have only " + hits + " hits so far");
-        } 
-           
-
+        }
     }
-    
-
 }
+
+var stats = "you took " + guesses + " guesses to sink the battleship " + "which means accuracy was " + (3 / guesses);
+alert(stats);
